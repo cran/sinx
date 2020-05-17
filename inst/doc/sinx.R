bd9184e457880ca4f758ff30599dc8770a7b7b12
Peng Zhao <pzhao@pzhao.net>
2020-05-17 06:40:02 +0000

    version 0.0.13

--- inst/doc/sinx.R
+++ inst/doc/sinx.R
@@ -1,4 +1,4 @@
-## ---- echo=FALSE, results='hide'-----------------------------------------
+## ---- echo=FALSE, results='hide'----------------------------------------------
 os <- Sys.info()['sysname']
 if(os == 'Windows') {
   old_loc <- Sys.getlocale("LC_CTYPE")
@@ -8,10 +8,10 @@ library("sinx")
 library("utils")
 f <- sinx:::merge_text(method = 'vig')
 
-## ---- results='asis', echo=FALSE-----------------------------------------
+## ---- results='asis', echo=FALSE----------------------------------------------
 cat(f$vig, sep = '\n\n\n\n')
 
-## ---- echo=FALSE, results='hide'-----------------------------------------
+## ---- echo=FALSE, results='hide'----------------------------------------------
 if(os == 'Windows') {
   Sys.setlocale("LC_CTYPE",old_loc)
 }
